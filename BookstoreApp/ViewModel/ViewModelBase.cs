3be04c32108af5a5f0507ff223f9e7a89164e4cf
Amanda Westerlind <amanda.westerlind@live.se>
2026-01-06 18:15:23 +0100

    Add classes ViewModelBase and (empty) MainWindowViewModel

---
+++ BookstoreApp/ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreApp.ViewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaiseProperyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

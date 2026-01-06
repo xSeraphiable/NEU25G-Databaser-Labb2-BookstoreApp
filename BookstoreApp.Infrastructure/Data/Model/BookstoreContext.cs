b4e4e70b3ef548aa486f218ffdbff6d9265275c0
Amanda Westerlind <amanda.westerlind@live.se>
2026-01-06 16:46:38 +0100

    Configure BookstoreContext to use SQL Server with User Secrets

--- BookstoreApp.Infrastructure/Data/Model/BookstoreContext.cs
+++ BookstoreApp.Infrastructure/Data/Model/BookstoreContext.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace BookstoreApp.Infrastructure;
 
@@ -34,9 +36,19 @@ public partial class BookstoreContext : DbContext
     public virtual DbSet<Store> Stores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Database=Bookstore;Integrated Security=True;TrustServerCertificate=True;");
+    {
+        var config = new ConfigurationBuilder().AddUserSecrets<BookstoreContext>().Build();
 
+        var connectionString = new SqlConnectionStringBuilder()
+        {
+            ServerSPN = config["ServerName"],
+            InitialCatalog = config["DatabaseName"],
+            TrustServerCertificate = true,
+            IntegratedSecurity = true
+        }.ToString();
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("Finnish_Swedish_CI_AS");

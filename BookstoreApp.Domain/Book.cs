27518d3187e2664f353c291b9c796807057f0b9f
Amanda Westerlind <amanda.westerlind@live.se>
2026-01-06 16:09:09 +0100

    Add project files.

---
+++ BookstoreApp.Domain/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookstoreApp.Infrastructure;
+
+public partial class Book
+{
+    public string Isbn { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Language { get; set; }
+
+    public int? Weight { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public DateOnly? ReleaseDate { get; set; }
+
+    public int? NumberOfPages { get; set; }
+
+    public decimal SalesPrice { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual ICollection<StockLevel> StockLevels { get; set; } = new List<StockLevel>();
+
+    public virtual ICollection<Author> Authors { get; set; } = new List<Author>();
+
+    public virtual ICollection<Genre> Genres { get; set; } = new List<Genre>();
+}
---
+++ BookstoreApp.Domain/StockLevel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookstoreApp.Infrastructure;
+
+public partial class StockLevel
+{
+    public string Isbn { get; set; } = null!;
+
+    public int StoreId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int QuantityOrdered { get; set; }
+
+    public decimal PurchasePrice { get; set; }
+
+    public virtual Book IsbnNavigation { get; set; } = null!;
+
+    public virtual Store Store { get; set; } = null!;
+}
